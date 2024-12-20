0b264e604bedb96536417b05f88ecde459d2cc88
Habi-Pyatha <unionhab@gmail.com>
2024-12-20 14:40:57 +0545

    folder structure change

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Favorites from './favorites'
-import Library from './library'
-import Trending from './trending'
-import Feed from './feed'
-import Player from './player'
+import Favorites from '../favorites'
+import Library from '../library'
+import Trending from '../trending'
+import Feed from '../feed'
+import Player from '../player'
+import './home.css'
+import Sidebar from '../../components/sidebar'
 function Home() {
     return (
         <Router>
+            <div className='main-body'>
+            < Sidebar/>
             <Routes>
                 <Route path="/" element={<Library/>} />
                 <Route path="/feed" element={<Feed/>} />
@@ -15,6 +19,7 @@ function Home() {
                 <Route path="/player" element={<Player/>} />
                 <Route path="/favorites" element={<Favorites/>} />
             </Routes>
+            </div>
         </Router>
         
     )
