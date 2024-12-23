f41fc908154c95a8549e91eed328aaad6bb72b9a
Habi-Pyatha <unionhab@gmail.com>
2024-12-23 13:22:36 +0545

    authorization token image

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,18 +1,46 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Favorites from '../favorites'
-import Library from '../library'
-import Trending from '../trending'
-import Feed from '../feed'
-import Player from '../player'
-import './home.css'
-import Sidebar from '../../components/sidebar'
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Favorites from "../favorites";
+import Library from "../library";
+import Trending from "../trending";
+import Feed from "../feed";
+import Player from "../player";
+import "./home.css";
+import Sidebar from "../../components/sidebar";
+import Login from "../auth/login";
+import { setClientToken } from "../../spotify";
 function Home() {
-    return (
-        <Router>
-            <div className='main-body'>
-            < Sidebar/>
-            <Routes>
+  const [token, setToken ] = useState("");
+  useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    const hash = window.location.hash;
+    window.location.hash= "";
+    if(!token && hash) {
+
+        const _token = hash.split("&")[0].split("=")[1];
+        window.localStorage.setItem("token", _token);
+        setToken(_token);
+        setClientToken(_token);
+    }else{
+        setToken(token);
+        setClientToken(token);
+
+    }
+
+    // if (localStorage.getItem("token")) {
+    //     setToken(localStorage.getItem("token"));
+    // }
+  }, []);
+
+  return !token ?(
+    <Login /> ) :
+    (
+
+    <Router>
+      <div className="main-body">
+        
+        < Sidebar/>
+        <Routes>
                 <Route path="/" element={<Library/>} />
                 <Route path="/feed" element={<Feed/>} />
                 <Route path="/trending" element={<Trending/>} />
@@ -20,10 +48,9 @@ function Home() {
                 <Route path="/favorites" element={<Favorites/>} />
                 <Route path="/library" element={<Library/>} />
             </Routes>
-            </div>
-        </Router>
-        
-    )
+      </div>
+    </Router>
+  );
 }
 
-export default Home
+export default Home;
