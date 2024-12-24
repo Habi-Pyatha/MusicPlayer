ba485eda498bd81a87301be45374d2b8ea3589b0
Habi-Pyatha <unionhab@gmail.com>
2024-12-24 16:53:16 +0545

    player page

--- src/screens/player/index.jsx
+++ src/screens/player/index.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import "./player.css";
+import { useLocation } from "react-router-dom";
+import apiClient from "../../spotify";
+import SongCard from "../../components/songCard";
+import Queue from "../../components/queue";
 function Player() {
-    return (
-        <div className='screen-container'>
-            player
-        </div>
-    )
+  const location = useLocation();
+  // console.log(location);
+  const [tracks, setTracks] = useState([]);
+  const [currentTrack, setCurrentTrack] = useState({});
+  const [currentIndex, setCurrentIndex] = useState(0);
+  useEffect(() => {
+    if (location.state) {
+      apiClient.get(`playlists/${location.state?.id}/tracks`).then((res) => {
+        // console.log(res.data.items);
+
+        setTracks(res.data.items);
+        setCurrentTrack(res.data.items[0]?.track);
+      });
+    }
+  }, [location.state]);
+  useEffect(() => {
+    // console.log("Current index updated:", currentIndex);
+    
+    setCurrentTrack(tracks[currentIndex]?.track);
+    // console.log("current track habi:",currentTrack);
+    
+    //   if (currentTrack && tracks[currentIndex]) {
+    //     }
+  }, [currentIndex, tracks]);
+
+  const hasAlbum= currentTrack && currentTrack.album;
+  return (
+    <div className="screen-container flex">
+      <div className="left-player-body"></div>
+      <div className="right-player-body">
+        <SongCard album={currentTrack?.album} />
+        <Queue tracks={tracks} setCurrentIndex={setCurrentIndex} />
+      </div>
+    </div>
+  );
 }
 
-export default Player
+export default Player;
