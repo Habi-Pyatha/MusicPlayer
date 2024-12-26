178f51df19292e087b001d1b11d11a7598fa0816
Habi-Pyatha <unionhab@gmail.com>
2024-12-26 18:54:43 +0545

    animation

--- src/screens/trending/index.jsx
+++ src/screens/trending/index.jsx
@@ -1,5 +1,9 @@
 import React, {useState} from 'react'
 import ReactAudioPlayer from 'react-audio-player';
+import { ImPrevious } from "react-icons/im";
+import { ImNext } from "react-icons/im";
+
+
 // import myAudioFiles from '../../songs';
 import './trending.css'
 const audioFiles = require.context("../../songs",false,/\.(mp3|wav|ogg)$/)
@@ -29,30 +33,95 @@ function Trending() {
                 <h2 className='song-title'>Trending Songs</h2>
                 <div className='song-lists'>
                 {songs.map((song,index)=>(
-                    <div key={index} onClick={()=>handleSongClick(index) } className='song-name'>
-                        {song.name}
+                    <div key={index}   >
+                        <span className='song-name' onClick={()=>handleSongClick(index) }     style={{
+                            cursor: 'pointer',
+                            margin: '10px 5px',
+                            color: currentSongIndex === index ? 'green' : 'blue',
+                        }}>
+                            {song.name}
+                        </span>
                         
                     </div>
                     
                 ))}
                 </div>
                 
-                <div className='player-controls'>
-                    
+                <div className='line-animations'>
+                <div class="now playing" id="music">
+                        <span class="bar n1">A</span>
+                        <span class="bar n2">B</span>
+                        <span class="bar n3">c</span>
+                        <span class="bar n4">D</span>
+                        <span class="bar n5">E</span>
+                        <span class="bar n6">F</span>
+                        <span class="bar n7">G</span>
+                        <span class="bar n8">H</span>
+                        <span class="bar n1">A</span>
+                        <span class="bar n2">B</span>
+                        <span class="bar n3">c</span>
+                        <span class="bar n4">D</span>
+                        <span class="bar n5">E</span>
+                        <span class="bar n6">F</span>
+                        <span class="bar n7">G</span>
+                        <span class="bar n8">H</span>
+                        <span class="bar n1">A</span>
+                        <span class="bar n2">B</span>
+                        <span class="bar n3">c</span>
+                        <span class="bar n4">D</span>
+                        <span class="bar n5">E</span>
+                        <span class="bar n6">F</span>
+                        <span class="bar n7">G</span>
+                        <span class="bar n8">H</span>
+                        <span class="bar n1">A</span>
+                        <span class="bar n2">B</span>
+                        <span class="bar n3">c</span>
+                        <span class="bar n4">D</span>
+                        <span class="bar n5">E</span>
+                        <span class="bar n6">F</span>
+                        <span class="bar n7">G</span>
+                        <span class="bar n8">H</span>
+                        <span class="bar n1">A</span>
+                        <span class="bar n2">B</span>
+                        <span class="bar n3">c</span>
+                        <span class="bar n4">D</span>
+                        <span class="bar n5">E</span>
+                        <span class="bar n6">F</span>
+                        <span class="bar n7">G</span>
+                        <span class="bar n8">H</span>
+                        <span class="bar n1">A</span>
+                        <span class="bar n2">B</span>
+                        <span class="bar n3">c</span>
+                        <span class="bar n4">D</span>
+                        <span class="bar n5">E</span>
+                        <span class="bar n6">F</span>
+                        <span class="bar n7">G</span>
+                        <span class="bar n8">H</span>
+                        <span class="bar n1">A</span>
+                        <span class="bar n2">B</span>
+                        <span class="bar n3">c</span>
+                        <span class="bar n4">D</span>
+                        <span class="bar n5">E</span>
+                        <span class="bar n6">F</span>
+                        
+                        </div>
+                        
                 </div>
                 <div className='audio-player'>
-                <button onClick={handlePrev} disabled={songs.length<=1}>Previous</button>
+                    <button onClick={handlePrev} disabled={songs.length<=1}><ImPrevious />
+                    </button>
 
-                <ReactAudioPlayer
-                            src={songs[currentSongIndex].path}
-                            autoPlay
-                            controls
-                            />
-                <button onClick={handleNext} disabled= {songs.legth<=1}>Next</button>
-
+                    <ReactAudioPlayer
+                                src={songs[currentSongIndex].path}
+                                autoPlay
+                                controls
+                                />
+                    <button onClick={handleNext} disabled= {songs.legth<=1}> <ImNext /></button>
+                    
                     
                 </div>
             </div>
+            
         </div>
     )
 }
